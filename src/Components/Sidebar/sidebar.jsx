c862051e31a4dc4303a521674875744cdff515cc
Behruz <bboisxonov@yandex.com>
2023-10-30 20:45:02 +0500

    sidebar desktop style finished

--- src/Components/Sidebar/sidebar.jsx
+++ src/Components/Sidebar/sidebar.jsx
@@ -1,12 +1,81 @@
 import React from 'react'
 
 import "./sidebar.css"
+import "../../global.css"
 
 function Sidebar(){
     return (
-    <section>
-        <h1>Sidebar</h1>
-    </section>
+        <div className='sidebar-fixed'>
+            <div className='sidebar'>
+                <img className='site-logo' src={require('../../images/site-logo.png')} alt="site logo (twitter)" />
+                
+                <ul className="sidenav">
+                    <li className="sidenav-item">
+                        <a className="sidenav-link" href="#">
+                            <img className='nav-icons' src={require('../../images/home-box.png')} alt="home-box" />
+                            Home
+                        </a>
+                    </li>
+                    <li className="sidenav-item">
+                        <a href="#" className="sidenav-link">
+                            <img className='nav-icons' src={require('../../images/explore.png')} alt="explore" />
+                            Explore
+                        </a>
+                    </li>
+                    <li className="sidenav-item">
+                        <a href="#" className="sidenav-link">
+                            <img className='nav-icons' src={require('../../images/notification.png')} alt="notification" />
+                            Notifications
+                        </a>
+                    </li>
+                    <li className="sidenav-item">
+                        <a href="#" className="sidenav-link">
+                            <img className='nav-icons' src={require('../../images/messages.png')} alt="messages" />
+                            Messages
+                        </a>
+                    </li>
+                    <li className="sidenav-item">
+                        <a href="#" className="sidenav-link">
+                            <img className='nav-icons' src={require('../../images/bookmarks.png')} alt="bookmarks" />
+                            Bookmarks
+                        </a>
+                    </li>
+                    <li className="sidenav-item">
+                        <a href="#" className="sidenav-link">
+                            <img className='nav-icons' src={require('../../images/lists.png')} altlists />
+                            Lists
+                        </a>
+                    </li>
+                    <li className="sidenav-item">
+                        <a href="#" className="sidenav-link">
+                            <img className='nav-icons' src={require('../../images/profile.png')} alt="profile" />
+                            Profile
+                        </a>
+                    </li>
+                    <li className="sidenav-item">
+                        <a href="#" className="sidenav-link">
+                            <img className='nav-icons' src={require('../../images/more.png')} alt="more btn" />
+                            More
+
+                        </a>
+                    </li>
+                </ul>
+                <button className='global-tweet-btn sidebar-btn'>Tweet</button>
+            </div>
+            <div className='users__profile'>
+                <img src={require('../../images/users__profile-img.png')} alt="" />
+
+                <div className="username">
+                    <h4>Bobur</h4>
+                    <p>@bobur_mavlonov</p>
+                </div>
+                <div className='tripple-dots'>
+                    <span className='users__profile-dots'></span>
+                    <span className='users__profile-dots'></span>
+                    <span className='users__profile-dots'></span>
+                </div>
+            </div>
+        </div>
     )
 }
 

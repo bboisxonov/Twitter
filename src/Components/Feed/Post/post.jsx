25900d69ecaf4cc45147ffee5a6a5dd1911cd777
Behruz <bboisxonov@yandex.com>
2023-11-01 20:12:20 +0500

    post component added

--- src/Components/Feed/Post/post.jsx
+++ src/Components/Feed/Post/post.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import "./post.css"
+import PostReactions from "../../Post-reactions/postreactions"
+
 function Post() {
 
-    return <>
-        <h2>Post</h2>
-    </>
+    return <div className='post-container'>
+        <div className="post">
+            <div className="post-info">
+                <img src={require('../../../images/creativePhoto.png')} alt="" />
+                <div className="post-text">
+                    <p><b>CreativePhoto</b> @cloutexhibition · 1h</p>
+                    <p className='post-caption'>Обетда..... <br /> Кечиринглар</p>
+                </div>
+            </div>
+            <div className='tripple-dots'>
+                <span className='users__profile-dots'></span>
+                <span className='users__profile-dots'></span>
+                <span className='users__profile-dots'></span>
+            </div>
+        </div>
+        <div className="post-image-box">
+            <img className='post-img' src={require('../../../images/user-post.png')} alt="" />
+        </div>
+            <PostReactions comments={"10"} retweet={5} likes={8}></PostReactions>
+    </div>
 }
 export default Post

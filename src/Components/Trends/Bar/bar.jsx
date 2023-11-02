3ca45ef86f075274b10fea51053005d0818f2b3c
Behruz <bboisxonov@yandex.com>
2023-11-02 15:58:03 +0500

    side bar component finished

--- src/Components/Trends/Bar/bar.jsx
+++ src/Components/Trends/Bar/bar.jsx
@@ -3,11 +3,54 @@ import React from 'react'
 import "./bar.css"
 
 function Bar(){
-    return (
-    <section>
-        <h1>Bar</h1>
-    </section>
-    )
+    return <div className="trendbar">
+        <ul className='bar'>
+
+            <li className='bar-item'>
+                <h3 className='bar-title'>Trends for you</h3>
+                <img src={require('../../../images/settings.png')} alt="" />
+            </li>
+
+            <li className='bar-item'>
+                <p className='bar-item__description'>Trending in Germany <br />
+                <b>Revolution</b> <br />
+                50.4K Tweets</p>
+
+                <div className='tripple-dots'>
+                    <span className='users__profile-dots'></span>
+                    <span className='users__profile-dots'></span>
+                    <span className='users__profile-dots'></span>
+                </div>
+            </li>
+
+            <li className='bar-item'>
+                <p className='bar-item__description'>Trending in Germany <br />
+                <b>Revolution</b> <br />
+                50.4K Tweets</p>
+
+                <div className='tripple-dots'>
+                    <span className='users__profile-dots'></span>
+                    <span className='users__profile-dots'></span>
+                    <span className='users__profile-dots'></span>
+                </div>
+            </li>
+
+            <li className='bar-item'>
+                <p className='bar-item__description'>Trending in Germany <br />
+                <b>Revolution</b> <br />
+                50.4K Tweets</p>
+
+                <div className='tripple-dots'>
+                    <span className='users__profile-dots'></span>
+                    <span className='users__profile-dots'></span>
+                    <span className='users__profile-dots'></span>
+                </div>
+            </li>
+
+            <h3 className='show-more'>Show more</h3>
+        </ul>
+
+    </div>
 }
 
 export default Bar

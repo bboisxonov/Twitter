ab9feabc5d361b752cec42f3dbc48bbe3132a228
Behruz <bboisxonov@yandex.com>
2023-11-02 15:41:28 +0500

    search component added

--- src/Components/Trends/Search/search.jsx
+++ src/Components/Trends/Search/search.jsx
@@ -3,11 +3,10 @@ import React from 'react'
 import "./search.css"
 
 function Search(){
-    return (
-    <section>
-        <h1>Search</h1>
-    </section>
-    )
+    return <div className='searchBox'>
+        <img className='search-icon' src={require('../../../images/search.png')} alt="search icon" />
+        <input className='search-input' type="text" placeholder='Search Twitter' />
+    </div>
 }
 
 export default Search
